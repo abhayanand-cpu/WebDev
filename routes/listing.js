b172b11e3eb29158d8a0867d940c6af857c7ea57
abhayanand-cpu <sidd.anand2000@gmail.com>
2025-01-02 00:54:03 +0530

    authentication

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,7 @@ const listing = require('../models/listing.js')
 const wrapAsync = require('../utils/wrapAsync.js');
 const expressError = require('../utils/expressError.js');
 const { listingSchema } = require('../schema.js');
+const { isloggedin } = require('../middleware.js');
 
 
 const validateListing = ( req, res, next ) => {
@@ -22,11 +23,11 @@ route.get("/", wrapAsync(async (req,res) => {
     res.render("listings/index.ejs",{allListing});
 }))
 
-route.get("/new",( req, res ) => {
+route.get("/new", isloggedin, (req, res) => {
     res.render("listings/new.ejs")
 })
 
-route.post("/",validateListing,wrapAsync(async ( req, res ) => {
+route.post("/",isloggedin, validateListing,wrapAsync(async ( req, res ) => {
     
     let listings = req.body.listing;
     let newData = new listing(listings);
@@ -36,27 +37,27 @@ route.post("/",validateListing,wrapAsync(async ( req, res ) => {
     res.redirect("/listings");
 }));
 
-route.get("/:id",wrapAsync(async ( req,res ) => {
+route.get("/:id", isloggedin, wrapAsync(async ( req,res ) => {
     let { id } = req.params;
     let data = await listing.findById(id).populate("reviews");
     if( !data ){
         req.flash('error','listing does not exist');
-        res.redirect("/listings");
+        return res.redirect("/listings");
     }
     res.render("listings/show.ejs",{ data })
 }))
 
-route.get("/:id/edit",wrapAsync(async ( req, res ) => {
+route.get("/:id/edit",isloggedin, wrapAsync(async ( req, res ) => {
     let { id } = req.params;
     let data = await listing.findById(id);
     if( !data ){
         req.flash('error','listing does not exist');
-        res.redirect("/listings");
+        return res.redirect("/listings");
     }
     res.render("listings/edit.ejs",{ data })
 }))
 
-route.put("/:id", validateListing, wrapAsync(async ( req, res ) => {
+route.put("/:id", isloggedin, validateListing, wrapAsync(async ( req, res ) => {
     let { id } = req.params;
     if( !req.body.listing ){
         throw new expressError(400, "send valid data for the listing")
@@ -68,7 +69,7 @@ route.put("/:id", validateListing, wrapAsync(async ( req, res ) => {
 }))
 
 //delete route
-route.delete("/:id",wrapAsync(async ( req, res ) => {
+route.delete("/:id", isloggedin, wrapAsync(async ( req, res ) => {
     let { id } = req.params;
     let deletedListing = await listing.findByIdAndDelete(id);
     req.flash('success','listing deleted!');
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,44 @@
 const express = require('express')
 const router = express.Router();
+const User = require('../models/user.js')
+const wrapAsync = require('../utils/wrapAsync.js');
+const { route } = require('./listing');
+const passport = require('passport');
 
 router.get("/signup",( req, res ) => {
-    // res.render("user/signup.ejs")
-    res.send("")
+    res.render("user/signup.ejs")
 });
 
+router.post("/signup",wrapAsync( async( req, res ) => {
+    try{
+        let { username, email, password } = req.body;
+        const newUser = new User({ username, email });
+        const registeredUser = await User.register(newUser, password);
+        req.flash("success","Welcome to wanderlust");
+        res.redirect("/listings");
+    }catch( err ){
+        req.flash("error",err.message);
+        res.redirect("/signup");
+    }
+}));
+
+router.get("/login",( req, res ) => {
+    res.render("user/login.ejs")
+});
+
+router.post("/login", passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), async (req, res) => {
+    req.flash("success", "Welcome back to Wanderlust");
+    res.redirect("/listings");
+});
+
+router.get("/logout", (req, res, next) => {
+    req.logOut((err) => {
+        if (err) {
+            return next(err);
+        }
+
+        req.flash("success", "you are logged out");
+        res.redirect("/listings");
+    })
+})
 module.exports = router;
